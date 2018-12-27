9b700eb7e19ea710423b7bd96054fb69e400d9a0
grigorashKlim <grigorash.klim@gmail.com>
2018-12-27 17:35:03 +0700

    v 0.9999999

--- sites/all/modules/simple_module/simple_module-template.tpl.php
+++ sites/all/modules/simple_module/simple_module-template.tpl.php
@@ -1,10 +1,8 @@
-<?php
-if (!isset($simple_module_form_array))
-{
-  echo "No records to display";
-  return false;
-}
- foreach ($simple_module_form_array as $delta)
- {
-   echo 'id: ' . $delta['string_id'] . ' string: ' . $delta['string'].'<br>';
- }
+<?php if (!isset($simple_module_form_array)): ?>
+  <?php print "No records to display"; ?>
+<?php else: ?>
+  <?php foreach ($simple_module_form_array as $delta): ?>
+    <?php print 'id: ' . $delta['string_id'] . ' string: ' . $delta['string'] . '<br>'; ?>
+  <?php endforeach; ?>
+<?php endif; ?>
+
